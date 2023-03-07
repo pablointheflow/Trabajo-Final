b95532ce017f2c456cc65163c896385d41087645
pablointheflow <pab_abad@hotmail.com>
2023-03-07 17:51:31 -0300

    UPDATE

--- js/ventana.js
+++ js/ventana.js
@@ -1,23 +1,30 @@
-const openContacto = document.querySelector('.contacto'); /* DESPLEGA EL FORM CUANDO PRESIONA EL LINK CONTACTO */
+ /* DESPLEGA EL FORM CUANDO PRESIONA EL LINK CONTACTO */
+const openContacto = document.querySelector('.contacto');
 const formulario = document.querySelector('.form');
-const closeContacto = document.querySelector('.form_submit'); /* CIERRA EL FORM CUANDO PRESIONA EL BOTON ENVIAR */
-const cancelar = document.querySelector('.cancelar'); /* CANCELA AL PRESIONAR EL BOTON CANCELAR */
+/* CIERRA EL FORM CUANDO PRESIONA EL BOTON ENVIAR */
+const closeContacto = document.querySelector('.form_submit');
+/* CIERRA AL PRESIONAR EL BOTON CANCELAR */ 
+const cancelar = document.querySelector('.cancelar'); 
 
-/* CAPTURA EL EVENTO CLICK PARA MOSTRAR U OCULTAR EL FORM */ 
+/* CAPTURA EL EVENTO CLICK PARA MOSTRAR EL FORM */ 
 openContacto.addEventListener('click', (e)=>{
     e.preventDefault();
-    formulario.classList.add('form--show'); /* AGREGA LA CLASE FORM--SHOW AL DIV PRINCIPAL PARA MOSTRAR EL FORMULARIO */
-});
+    /* AGREGA LA CLASE FORM--SHOW AL DIV PRINCIPAL PARA MOSTRAR EL FORMULARIO */
+    formulario.classList.add('form--show'); });
 
+/* CAPTURA EL EVENTO CLICK PARA CERRAR EL FORM */ 
 closeContacto.addEventListener('click', (e)=>{
     e.preventDefault();
     alert('Mensaje enviado');
-    formulario.classList.remove('form--show'); /* REMUEVE LA CLASE FORM--SHOW PARA CERRAR EL FORM */
+     /* REMUEVE LA CLASE FORM--SHOW PARA CERRAR EL FORM */
+    formulario.classList.remove('form--show');
 });
 
+/* EVENTO CANCELAR */ 
 cancelar.addEventListener('click', (e)=>{
     e.preventDefault();
-    formulario.classList.remove('form--show'); /* REMUEVE LA CLASE FORM--SHOW PARA CERRAR EL FORM */
+    /* REMUEVE LA CLASE FORM--SHOW PARA CERRAR EL FORM */
+    formulario.classList.remove('form--show'); 
 });
 
 
